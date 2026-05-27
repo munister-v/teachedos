5562aa839a19786c1d887a81cff23048093aa90d
Vyacheslaw Munister <vyacheslawmunister@MacBook-Air-Vyacheslaw.local>
2026-05-27 18:41:25 +0300

    Make emoji stickers place like Miro

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'teachedos-v69';
+const CACHE = 'teachedos-v70';
 const BASE_PATH = new URL(self.registration.scope).pathname;
 const base = path => new URL(path, self.registration.scope).pathname;
 
