2691cd25e9b800ee71ac20956d84e201279bf69d
Vyacheslaw Munister <vyacheslawmunister@MacBook-Air-Vyacheslaw.local>
2026-05-27 21:07:24 +0300

    Polish Miro-style board tools

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'teachedos-v70';
+const CACHE = 'teachedos-v71';
 const BASE_PATH = new URL(self.registration.scope).pathname;
 const base = path => new URL(path, self.registration.scope).pathname;
 
