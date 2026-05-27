e3e3d5df4a9f07d4f25ea97a87179b62cd6bd770
Vyacheslaw Munister <vyacheslawmunister@MacBook-Air-Vyacheslaw.local>
2026-05-27 18:31:06 +0300

    Turn board tools into constructors

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'teachedos-v68';
+const CACHE = 'teachedos-v69';
 const BASE_PATH = new URL(self.registration.scope).pathname;
 const base = path => new URL(path, self.registration.scope).pathname;
 
