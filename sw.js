788b6c88cdb35e2443c4634c825525e9883ec438
Vyacheslaw Munister <munister@outlook.com>
2026-08-01 12:00:09 +0300

    Refine lesson pack board visuals

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'teachedos-v248';
-const VERSION = '248';
+const CACHE = 'teachedos-v249';
+const VERSION = '249';
 const BASE_PATH = new URL(self.registration.scope).pathname;
 const base = path => new URL(path, self.registration.scope).pathname;
 
