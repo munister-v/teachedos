68db454ece73b37d57a9a540e5d4024d39ed14d0
Vyacheslaw Munister <munister@outlook.com>
2026-08-10 11:39:33 +0300

    feat(homework): prioritize review workflow

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'teachedos-v258';
-const VERSION = '258';
+const CACHE = 'teachedos-v259';
+const VERSION = '259';
 const BASE_PATH = new URL(self.registration.scope).pathname;
 const base = path => new URL(path, self.registration.scope).pathname;
 
