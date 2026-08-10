4d8ab790bf090ed2c06ecf6de6568412d5958431
Vyacheslaw Munister <munister@outlook.com>
2026-08-10 17:02:53 +0300

    fix(schedule): make navigation and actions reachable

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'teachedos-v269';
-const VERSION = '269';
+const CACHE = 'teachedos-v270';
+const VERSION = '270';
 const BASE_PATH = new URL(self.registration.scope).pathname;
 const base = path => new URL(path, self.registration.scope).pathname;
 
