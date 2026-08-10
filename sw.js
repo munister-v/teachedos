d24930e50c5bbe9bd936a8511e995d32b1a0036c
Vyacheslaw Munister <munister@outlook.com>
2026-08-10 17:08:49 +0300

    fix(board): align classroom canvas with lesson cards

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'teachedos-v270';
-const VERSION = '270';
+const CACHE = 'teachedos-v271';
+const VERSION = '271';
 const BASE_PATH = new URL(self.registration.scope).pathname;
 const base = path => new URL(path, self.registration.scope).pathname;
 
