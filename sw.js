1f764fe7d4f912a390eae5ae29ad06a0cecce7e5
Vyacheslaw Munister <munister@outlook.com>
2026-08-10 11:00:18 +0300

    feat(gradebook): add progress focus and student filters

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'teachedos-v254';
-const VERSION = '254';
+const CACHE = 'teachedos-v255';
+const VERSION = '255';
 const BASE_PATH = new URL(self.registration.scope).pathname;
 const base = path => new URL(path, self.registration.scope).pathname;
 
