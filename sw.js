7757a6d0ea6e6d2a8d91d6dd0633aacb6ad955b4
Vyacheslaw Munister <munister@outlook.com>
2026-08-18 10:48:33 +0300

    site: internal links point at the canonical root, not the second URL of the same page

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'teachedos-v318';
-const VERSION = '318';
+const CACHE = 'teachedos-v319';
+const VERSION = '319';
 const BASE_PATH = new URL(self.registration.scope).pathname;
 const base = path => new URL(path, self.registration.scope).pathname;
 
