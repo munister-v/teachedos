d4871dbc9afda652dd6b8edec573e3f9aba3eb1f
Vyacheslaw Munister <munister@outlook.com>
2026-08-23 02:37:58 +0300

    Bump asset version to 421 after board Lesson Collector wiring

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'teachedos-v420';
-const VERSION = '420';
+const CACHE = 'teachedos-v421';
+const VERSION = '421';
 const BASE_PATH = new URL(self.registration.scope).pathname;
 const base = path => new URL(path, self.registration.scope).pathname;
 
