dfdc9d3089520926337f778c36c51eb605ec9930
Vyacheslaw Munister <munister@outlook.com>
2026-08-23 11:26:31 +0300

    Bump to v432 via scripts/bump-version.mjs after stripe removal

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'teachedos-v431';
-const VERSION = '431';
+const CACHE = 'teachedos-v432';
+const VERSION = '432';
 const BASE_PATH = new URL(self.registration.scope).pathname;
 const base = path => new URL(path, self.registration.scope).pathname;
 
