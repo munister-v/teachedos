22cd97bb012bed21d97481c4844cf933f8cbc20d
Vyacheslaw Munister <munister@outlook.com>
2026-08-23 10:24:24 +0300

    Bump asset version to 426 after Teaching Tools catalog sync

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'teachedos-v425';
-const VERSION = '425';
+const CACHE = 'teachedos-v426';
+const VERSION = '426';
 const BASE_PATH = new URL(self.registration.scope).pathname;
 const base = path => new URL(path, self.registration.scope).pathname;
 
